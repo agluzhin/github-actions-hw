67dfc6b6128be81ba8df588c37302f5bd1735e08
agluzhin <agluzhin@gmail.com>
2025-12-12 15:04:21 +0300

    Initial commit: base Spring Boot application structure with preset Dockerfile

---
+++ src/main/java/agluzhin/github_actions_hw/GithubActionsHwApplication.java
@@ -0,0 +1,13 @@
+package agluzhin.github_actions_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/agluzhin/github_actions_hw/GithubActionsHwApplicationTests.java
@@ -0,0 +1,13 @@
+package agluzhin.github_actions_hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
